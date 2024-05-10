1321d2dba36cc7eed690c630c274c8e5e14cc700
metafates <vvz.dev@gmail.com>
2024-05-10 11:43:34 +0300

    refactor: make it better

--- brewer_term/src/cli.rs
+++ brewer_term/src/cli.rs
@@ -1,7 +1,14 @@
+use std::io::{BufWriter, Write};
+
 use clap::{Parser, Subcommand};
+use terminal_size::{terminal_size, Width};
+
+use brewer_engine::{Engine, State};
+
+use crate::pretty;
 
 #[derive(Parser)]
-#[command(version, about, styles = get_styles())]
+#[command(version, about)]
 pub struct Cli {
     #[command(subcommand)]
     pub command: Commands,
@@ -13,10 +20,10 @@ pub enum Commands {
     Which(Which),
 
     /// Update the local cache
-    Update,
+    Update(Update),
 
     /// List installed formulae and casks
-    List,
+    List(List),
 }
 
 #[derive(Parser)]
@@ -24,40 +31,111 @@ pub struct Which {
     pub name: String,
 }
 
-pub fn get_styles() -> clap::builder::Styles {
-    clap::builder::Styles::styled()
-        .usage(
-            anstyle::Style::new()
-                .bold()
-                .underline()
-                .fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::Yellow))),
-        )
-        .header(
-            anstyle::Style::new()
-                .bold()
-                .underline()
-                .fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::Yellow))),
-        )
-        .literal(
-            anstyle::Style::new().fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::Green))),
-        )
-        .invalid(
-            anstyle::Style::new()
-                .bold()
-                .fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::Red))),
-        )
-        .error(
-            anstyle::Style::new()
-                .bold()
-                .fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::Red))),
-        )
-        .valid(
-            anstyle::Style::new()
-                .bold()
-                .underline()
-                .fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::Green))),
-        )
-        .placeholder(
-            anstyle::Style::new().fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::White))),
-        )
+impl Which {
+    pub fn run(&self, state: State) -> anyhow::Result<bool> {
+        let suitable: Vec<_> = state
+            .formulae
+            .all
+            .into_iter()
+            .filter_map(|(_, f)| {
+                if f.executables.contains(&self.name) {
+                    Some(f)
+                } else {
+                    None
+                }
+            })
+            .collect();
+
+        if suitable.is_empty() {
+            return Ok(false);
+        }
+
+        let mut buf = BufWriter::new(std::io::stdout());
+
+        for (i, f) in suitable.iter().enumerate() {
+            writeln!(buf, "{} {}", f.base.name, f.base.versions.stable)?;
+            writeln!(buf, "{}", f.base.desc)?;
+
+            if i != suitable.len() - 1 {
+                writeln!(buf)?;
+            }
+        }
+
+        buf.flush()?;
+
+        Ok(true)
+    }
+}
+
+#[derive(Parser)]
+pub struct Update {}
+
+impl Update {
+    pub fn run(&self, mut engine: Engine) -> anyhow::Result<()> {
+        let state = engine.latest()?;
+
+        engine.update_cache(&state)?;
+
+        println!("Updated, found {} formulae and {} casks", state.formulae.all.len(), state.casks.all.len());
+
+        Ok(())
+    }
+}
+
+#[derive(Parser)]
+pub struct List {}
+
+impl List {
+    pub fn run(&self, state: State) -> anyhow::Result<()> {
+        let mut buf = BufWriter::new(std::io::stdout());
+
+        let max_width = terminal_size().map(|(Width(w), _)| w).unwrap_or(80);
+
+        {
+            writeln!(buf, "{}", pretty::header("Formulae"))?;
+
+            let mut installed: Vec<_> = state
+                .formulae
+                .installed
+                .into_values()
+                .filter_map(|f| {
+                    if f.receipt.installed_on_request {
+                        Some(f.upstream.base.name)
+                    } else {
+                        None
+                    }
+                })
+                .collect();
+
+            installed.sort_unstable();
+
+            let table = pretty::table(&installed, max_width);
+
+            table.print(&mut buf)?;
+
+            if !installed.is_empty() {
+                writeln!(buf)?;
+            }
+        }
+        {
+            writeln!(buf, "{}", pretty::header("Casks"))?;
+
+            let mut installed: Vec<_> = state
+                .casks
+                .installed
+                .into_values()
+                .map(|v| v.upstream.base.token)
+                .collect();
+
+            installed.sort_unstable();
+
+            let table = pretty::table(&installed, max_width);
+
+            table.print(&mut buf)?;
+        }
+
+        buf.flush()?;
+
+        Ok(())
+    }
 }
--- brewer_term/src/main.rs
+++ brewer_term/src/main.rs
@@ -1,13 +1,11 @@
-use std::io::{BufWriter, Write};
 use std::path::PathBuf;
 use std::process::exit;
 
 use clap::Parser;
-use terminal_size::{terminal_size, Width};
 
-use brewer_engine::{Engine, State};
+use brewer_engine::Engine;
 
-use crate::cli::{Cli, Commands, Which};
+use crate::cli::{Cli, Commands};
 
 mod cli;
 mod pretty;
@@ -16,24 +14,24 @@ fn run() -> anyhow::Result<bool> {
     let c = Cli::parse();
 
     match c.command {
-        Commands::Which(which) => {
+        Commands::Which(cmd) => {
             let mut engine = get_engine()?;
             let state = engine.cache_or_latest()?;
 
-            Ok(cmd_which(which, state)?)
+            Ok(cmd.run(state)?)
         }
-        Commands::Update => {
+        Commands::Update(cmd) => {
             let engine = get_engine()?;
 
-            cmd_update(engine)?;
+            cmd.run(engine)?;
 
             Ok(true)
         }
-        Commands::List => {
+        Commands::List(cmd) => {
             let mut engine = get_engine()?;
             let state = engine.cache_or_latest()?;
 
-            cmd_list(state)?;
+            cmd.run(state)?;
 
             Ok(true)
         }
@@ -48,103 +46,6 @@ fn get_engine() -> anyhow::Result<Engine> {
         .build()?;
 
     Ok(engine)
-}
-
-fn cmd_list(state: State) -> anyhow::Result<()> {
-    let mut buf = BufWriter::new(std::io::stdout());
-
-    let max_width = terminal_size().map(|(Width(w), _)| w).unwrap_or(80);
-
-    {
-        writeln!(buf, "{}", pretty::header("Formulae"))?;
-
-        let mut installed: Vec<_> = state
-            .formulae
-            .installed
-            .into_values()
-            .filter_map(|f| {
-                if f.receipt.installed_on_request {
-                    Some(f.upstream.base.name)
-                } else {
-                    None
-                }
-            })
-            .collect();
-
-        installed.sort_unstable();
-
-        let table = pretty::table(&installed, max_width);
-
-        table.print(&mut buf)?;
-
-        if !installed.is_empty() {
-            writeln!(buf)?;
-        }
-    }
-    {
-        writeln!(buf, "{}", pretty::header("Casks"))?;
-
-        let mut installed: Vec<_> = state
-            .casks
-            .installed
-            .into_values()
-            .map(|v| v.upstream.base.token)
-            .collect();
-
-        installed.sort_unstable();
-
-        let table = pretty::table(&installed, max_width);
-
-        table.print(&mut buf)?;
-    }
-
-    buf.flush()?;
-
-    Ok(())
-}
-
-fn cmd_update(mut engine: Engine) -> anyhow::Result<()> {
-    let state = engine.latest()?;
-
-    engine.update_cache(&state)?;
-
-    println!("Updated, found {} formulae and {} casks", state.formulae.all.len(), state.casks.all.len());
-
-    Ok(())
-}
-
-fn cmd_which(args: Which, state: State) -> anyhow::Result<bool> {
-    let suitable: Vec<_> = state
-        .formulae
-        .all
-        .into_iter()
-        .filter_map(|(_, f)| {
-            if f.executables.contains(&args.name) {
-                Some(f)
-            } else {
-                None
-            }
-        })
-        .collect();
-
-    if suitable.is_empty() {
-        return Ok(false);
-    }
-
-    let mut buf = BufWriter::new(std::io::stdout());
-
-    for (i, f) in suitable.iter().enumerate() {
-        writeln!(buf, "{} {}", f.base.name, f.base.versions.stable)?;
-        writeln!(buf, "{}", f.base.desc)?;
-
-        if i != suitable.len() - 1 {
-            writeln!(buf)?;
-        }
-    }
-
-    buf.flush()?;
-
-    Ok(true)
 }
 
 fn main() {
